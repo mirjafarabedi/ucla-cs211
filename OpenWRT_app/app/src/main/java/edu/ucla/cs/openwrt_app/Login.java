83a8748c954dd9f5dc3ac79cd472510507c732dc
exastiken <jeffreychen.online@gmail.com>
2016-02-25 07:40:14 -0800

    App build
    added UI

---
+++ OpenWRT_app/app/src/main/java/edu/ucla/cs/openwrt_app/Login.java
@@ -0,0 +1,13 @@
+package edu.ucla.cs.openwrt_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//login screen
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
