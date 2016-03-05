ecea2d4dbf1c328e15cf44879fe7ecdc26ba0b26
zhehaowang <wangzhehao410305@gmail.com>
2016-03-04 16:16:53 -0800

    AppFilterServer: remove asio dependency

--- appfilter-server/appfilter_server.py
+++ appfilter-server/appfilter_server.py
@@ -2,11 +2,6 @@ import time
 import subprocess
 import select
 
-try:
-    import asyncio
-except ImportError:
-    import trollius as asyncio
-
 class AppFilterServer(object):
   def __init__(self):
     return
@@ -15,7 +10,6 @@ class AppFilterServer(object):
     print line
     return
 
-  @asyncio.coroutine
   def followFile(self, filename):
     f = subprocess.Popen(['tail','-F', filename],\
           stdout = subprocess.PIPE,stderr = subprocess.PIPE)
@@ -26,9 +20,7 @@ class AppFilterServer(object):
         if p.poll(1):
             self.getStat(f.stdout.readline())
         time.sleep(0.01)
-        yield None
 
 if __name__ == "__main__":
   appFilterServer = AppFilterServer()
-  loop = asyncio.get_event_loop()
-  loop.run_until_complete(appFilterServer.followFile("log.txt"))
+  appFilterServer.followFile("log.txt")
